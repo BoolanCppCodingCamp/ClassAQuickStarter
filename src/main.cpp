3a3e130de4a9ab0632604f38e54f0cd1846f91fa
Wenbo Yang <wenbo.yangcn@gmail.com>
2022-05-01 02:53:06 -0400

    Add catch2 test

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,15 @@
 #include <iostream>
 
+class A {
+    int a = 1;
+    int b = 2;
+    int c = 3;
+public:
+    A(){ std::cout << "A ctor" << std::endl; }
+};
+
 int main(int, char**) {
+    A a{};
+
     std::cout << "Hello, world!\n";
 }
